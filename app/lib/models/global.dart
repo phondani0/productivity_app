749cd2258a84ca0060d54a30b386bf8ee40fd29e
Ankit Phondani <ankitphondani52@gmail.com>
2020-09-25 00:55:30 +0530

    Initial commit

---
+++ app/lib/models/global.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+Color darkGreyColor = new Color(0xFF21212B);
+
+TextStyle intrayTitleStyle = new TextStyle(
+  fontFamily: 'Avenir',
+  fontWeight: FontWeight.bold,
+  color: Colors.grey[850],
+  fontSize: 40.0,
+);
